b0cf72984b938ca0192c50c50e5731398c12b3f3
ogsletcax <caxarde@ogsl.ca>
2022-12-12 16:36:39 -0500

    add text highlight and tag search

---
+++ website/components/Highlight.vue
@@ -0,0 +1,24 @@
+<script setup>
+const props = defineProps({
+    content: String,
+    query: String
+})
+
+function highlightContent() {
+    if (!this.props.query) return this.props.content;
+
+    return this.props.content.replace(new RegExp(this.props.query, "gi"), match => {
+        return '<span class="highlighted-text">' + match + '</span>';
+    });
+}
+</script>
+
+<template>
+    <div v-html="highlightContent()"></div>
+</template>
+
+<style scoped>
+:deep(.highlighted-text) {
+    background: yellow;
+}
+</style>
